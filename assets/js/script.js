ba204e4b336b762e0979c678fe578105971920c3
dillin92 <dillin970@gmail.com>
2021-05-24 02:39:57 -0400

    added js functionality to search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,40 @@
 var userSearch = document.querySelector('#search');
+var searchBtn = document.querySelector('#searchBtn');
+var currentCity = document.querySelector('#name');
+var currentWeatherDisplay =document.querySelector('#display');
 
 
 
 
-userSearch.addEventListener('submit');
 
 
+function displayCurrentWeather(data) {
 
 
+    var name= data.name;
+    var temp= data.main.temp;
+    var feels_like= data.main.feels_like;
+    var humidity= data.main.humidity;
+    var currentWeather = temp, feels_like, humidity;
 
+    console.log(name, temp, feels_like, humidity);
+
+    var latLon = data.coord;
+    console.log(latLon);
+
+    currentCity.innerHTML = name;
+    currentWeatherDisplay.innerHTML = "TEMPERATURE:" + currentWeather;
+
+
+};
+
+
+searchBtn.addEventListener('click', function getCurrentWeather() {
+    var userCity = userSearch.value.trim();
+    console.log(userCity);
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&units=imperial&appid=e3a955cb151dd35974130ff7ccec57c6').then(repsonse => repsonse.json()).then(function(data) {
+        console.log(data);
+        displayCurrentWeather(data);
+    });
+});
