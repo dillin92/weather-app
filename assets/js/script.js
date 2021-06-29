6455f6d7617f4a41530d5c51599efa46b344bc30
dillin92 <dillin970@gmail.com>
2021-06-28 23:33:30 -0400

    built current and forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,6 @@ var currentCity = document.querySelector('#name');
 var currentWeatherDisplay =document.querySelector('#display');
 var innerCardBody = document.querySelector('#display');
 
-
-
-
-
-
-
-
 searchBtn.addEventListener('click', function getCurrentWeather() {
     var userCity = userSearch.value.trim();
     console.log(userCity);
@@ -30,26 +23,74 @@ searchBtn.addEventListener('click', function getCurrentWeather() {
             var temp = data.current.temp.toFixed(0);
             var feels_like = data.current.feels_like.toFixed(0);
             var humidity = data.current.humidity.toFixed(0);
-            let locationIcon = document.querySelector("#icon");
+            var uvi = data.current.uvi;
+            let currentWindspeed = data.current.wind_speed.toFixed(0);
+
+            let timestamp = data.current.dt;
+            let dateObj = new Date(timestamp * 1000);
+            let month = dateObj.getMonth() + 1;
+            let dateVar = dateObj.getDate();
+            let year = dateObj.getFullYear();
+            let date = month + "/" + dateVar + "/" + year;
             
             const iconCode = data.current.weather[0].icon;
             const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-            locationIcon.setAttribute("src", iconUrl);
-            console.log(data, icon);
+       
+     
+
+            var card = document.createElement('div')
+                card.className = 'card';
+                var cardDivider = document.createElement('div')
+                cardDivider.className = 'card-divider';
+                var cardBody = document.createElement('div')
+                cardBody.className = 'card-body text-white';
+                 
+                card.appendChild(cardDivider);
+                card.appendChild(cardBody);
+
+                var dateEl = document.createElement('h1');
+                dateEl.className = 'date';
+                dateEl.setAttribute('id', 'date');
+                dateEl.textContent = date;
+                cardDivider.appendChild(dateEl);
 
 
+                var forecastIconEl = document.createElement('img');
+                forecastIconEl.className = 'icon';
+                forecastIconEl.setAttribute('src', iconUrl);
+                forecastIconEl.textContent = "Temp:" + temp + '°F';
+                cardDivider.appendChild(forecastIconEl);
+               
+    
+     
+                var forecastMaxTempEl = document.createElement('h3')
+                forecastMaxTempEl.className = 'max-temp';
+                forecastMaxTempEl.setAttribute('id', 'max-temp');
+                forecastMaxTempEl.textContent = "Feels Like:" + feels_like + '°F';
+                cardDivider.appendChild(forecastMaxTempEl);
+            
+                var forecastHumidityEl = document.createElement('h2')
+                forecastHumidityEl.className = 'min-temp';
+                forecastHumidityEl.setAttribute('id', 'min-temp');
+                forecastHumidityEl.textContent = "Humidity:" + humidity + '%';
+                cardDivider.appendChild(forecastHumidityEl);
 
-            currentCity.textContent = name;
-            var cityTemp = document.querySelector('#temp');
-            cityTemp.textContent = "Temperature:" + temp + '°F';
-            var cityFeelsLike = document.querySelector('#feels-like');
-            cityFeelsLike.textContent = "Feels Like:" + feels_like + '°F';
-            var cityHumidity = document.querySelector('#humidity');
-            cityHumidity.textContent = "Humidity:" + humidity + "%";
+                var forecastWindSpeedEl = document.createElement('h2')
+                forecastWindSpeedEl.classname = "wind-speed";
+                forecastWindSpeedEl.setAttribute("id", "wind-speed");
+                forecastWindSpeedEl.textContent = "Wind-speed:" + currentWindspeed + "MPH";
+                cardDivider.appendChild(forecastWindSpeedEl);
+
+                var currentUviEl = document.createElement('h2')
+                currentUviEl.classname = "uvi";
+                currentUviEl.setAttribute("id", "uvi");
+                currentUviEl.textContent = uvi;
+                cardDivider.appendChild(currentUviEl);
+
+                innerCardBody.appendChild(card);
 
 
-
-
+           
         });
        
     });
@@ -73,15 +114,20 @@ searchBtn.addEventListener('click', function getForecast() {
             //Daily Weather Card's
 
             for (let i = 0; i < 5 && i < data.daily.length; i++) {
-                var day = data.daily[i];
-                // var date = data.daily[i].dt;
-                // var convertedDate = new Date (date *1000);
-                // document.textContent(convertedDate.toGMTString()+toLocaleString());
-                // console.log(day, date, convertedDate);
+                var day = data.daily[i + 1];
+                
+                let timestamp = day.dt;
+                let dateObj = new Date(timestamp * 1000);
+                let month = dateObj.getMonth() + 1;
+                let dateVar = dateObj.getDate();
+                let year = dateObj.getFullYear();
+                let date = month + "/" + dateVar + "/" + year;
+
                 
                 var maxTemp = day.temp.max.toFixed(0);
                 var minTemp = day.temp.min.toFixed(0);
                 var humidity = day.humidity.toFixed(0);
+                var windspeed = day.wind_speed.toFixed(0);
 
                 const iconCode = day.weather[0].icon;
                 const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
@@ -97,11 +143,11 @@ searchBtn.addEventListener('click', function getForecast() {
                 card.appendChild(cardDivider);
                 card.appendChild(cardBody);
 
-                // var date = document.createElement('h1');
-                // date.className = 'date';
-                // date.setAttribute('id', 'date');
-                // date.textContent = date;
-                // cardDivider.appendChild(date);
+                var dateEl = document.createElement('h1');
+                dateEl.className = 'date';
+                dateEl.setAttribute('id', 'date');
+                dateEl.textContent = date;
+                cardDivider.appendChild(dateEl);
 
 
                 var forecastIconEl = document.createElement('img');
@@ -122,7 +168,7 @@ searchBtn.addEventListener('click', function getForecast() {
                 var forecastMinTempEl = document.createElement('h2')
                 forecastMinTempEl.className = 'min-temp';
                 forecastMinTempEl.setAttribute('id', 'min-temp');
-                forecastMinTempEl.textContent = "High:" + minTemp + '°F';
+                forecastMinTempEl.textContent = "Low:" + minTemp + '°F';
                 cardDivider.appendChild(forecastMinTempEl);
        
 
@@ -131,6 +177,12 @@ searchBtn.addEventListener('click', function getForecast() {
                 forecastHumidityEl.setAttribute('id', 'min-temp');
                 forecastHumidityEl.textContent = "Humidity:" + humidity + '%';
                 cardDivider.appendChild(forecastHumidityEl);
+
+                var forecastWindSpeedEl = document.createElement('h2')
+                forecastWindSpeedEl.classname = "wind-speed";
+                forecastWindSpeedEl.setAttribute("id", "wind-speed");
+                forecastWindSpeedEl.textContent = "Wind-speed:" + windspeed + "MPH";
+                cardDivider.appendChild(forecastWindSpeedEl);
 
                 innerCardBody.appendChild(card);
             
